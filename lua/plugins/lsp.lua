f8a7296457bcc1aa6810985a27dd11f7781e88ea
afazio1 <alexa.fazio04@gmail.com>
2024-12-15 15:12:31 -0500

    lsp keymaps + autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,32 @@
 return {
   'neovim/nvim-lspconfig',
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
   config = function()
-    require'lspconfig'.clangd.setup{}
-    require'lspconfig'.gopls.setup{}
-    require'lspconfig'.pyright.setup{}
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lsp_keymaps = function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = 0})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
+      vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
+      vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer = 0})
+      vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer = 0})
+      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer = 0})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer = 0})
+    end
+    require'lspconfig'.clangd.setup{
+      capabilities = capabilities,
+      on_attach = lsp_keymaps,
+    }
+    require'lspconfig'.gopls.setup{
+      capabilities = capabilities,
+      on_attach = lsp_keymaps,
+    }
+    require'lspconfig'.pyright.setup{
+      capabilities = capabilities,
+      on_attach = lsp_keymaps,
+    }
   end,
 }
