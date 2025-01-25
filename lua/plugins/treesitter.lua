31f7be40bdbb4db5c1be7e0978dbbf94deae6ca1
afazio1 <alexa.fazio04@gmail.com>
2025-01-25 13:55:34 -0500

    tsx support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "typescript", "javascript", "html", "go", "markdown", "cpp", "python"},
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "html", "css", "go", "markdown", "cpp", "python"},
       sync_install = false,
       highlight = {
         enable = true,
