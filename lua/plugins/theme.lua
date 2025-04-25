bb2660c1d5b885d42147315253186fa407804aff
afazio1 <alexa.fazio04@gmail.com>
2025-04-25 00:02:37 -0400

    add transparency to theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,13 @@ return {
     priority = 1000,
     opts = {},
     config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        }
+      })
       vim.cmd[[colorscheme tokyonight-night]]
     end,
   },
