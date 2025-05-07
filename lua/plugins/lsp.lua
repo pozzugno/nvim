053612e4a95e74e078f63fff07a77a01dd437661
afazio1 <alexa.fazio04@gmail.com>
2025-05-07 10:15:33 -0400

    add goto declaration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
     local lsp_keymaps = function()
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {buffer = 0})
       vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = 0})
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
       vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
